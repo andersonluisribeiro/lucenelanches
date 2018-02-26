a75fa33e6f1fdfc595779d50c77beed965396cd8
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2018-02-26 11:23:25 -0300

    atualizacoes de ambiente?

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ clear
 
 curl -fsSL https://get.docker.com | sh;
 
-curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
